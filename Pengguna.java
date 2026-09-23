29f7a5a026fa608cbf4f5d7a8e4ce559419c6731
yahyajailani <yahyajailani363@gmail.com>
2026-09-23 19:53:56 +0800

    update sistem bioskop

---
+++ Pengguna.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADVAN
+ */
+public abstract class Pengguna {
+
+    private String nama;
+    private String email;
+    private String noHP;
+
+    public Pengguna(String nama, String email, String noHP) {
+        this.nama = nama;
+        this.email = email;
+        this.noHP = noHP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNoHP() {
+        return noHP;
+    }
+
+    public void setNoHP(String noHP) {
+        this.noHP = noHP;
+    }
+
+    public abstract String getRole();
+}
